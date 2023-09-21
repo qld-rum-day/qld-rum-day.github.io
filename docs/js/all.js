4ac63264011959a17eb49b2f3ccd5b4d445fdfce
Nik Nieuwenhuis <nik@thevariousartists.com.au>
2023-09-21 11:57:21 +1000

    update

--- docs/js/all.js
+++ docs/js/all.js
@@ -14,3 +14,21 @@ new simpleParallax(image, {
 	delay: .6,
 	transition: 'cubic-bezier(0,0,0,1)'
 });
+
+
+// Age Gate Modal using cookies to only show once.
+
+const myModal = new bootstrap.Modal('#agegate');
+const myModalEl = document.getElementById('agegate')
+
+if (!Cookies.get('popup')) {
+    myModal.show();
+}
+
+myModalEl.addEventListener('hidden.bs.modal', event => {
+  Cookies.set('popup', 'valid', { expires: 3, path: "/" });
+})
+
+
+
+
